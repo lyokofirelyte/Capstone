73433f74199469d9485d57bbc5bce81aa8f47749
unknown <dtossber@PC03280N1.student.puc.purduecal.edu>
2013-11-21 13:48:48 -0600

    Update

--- util/scripts.js
+++ util/scripts.js
@@ -1,1 +1,15 @@
 /* Scripts! */
+
+function init(){
+document.getElementById("navHome").onmouseover = homeMenuHover("navHome");
+}
+
+function homeBanner(){
+
+}
+
+function homeMenuHover(x){
+
+alert(x);
+
+}
